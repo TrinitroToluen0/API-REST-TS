dd746ee570f7c18f20d27905d0c8efc06ca0496f
TrinitroToluen0 <javiermenco404@gmail.com>
2024-09-22 18:22:38 -0500

    Avoid hardcoding the URL

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { connectDB } from "./db";
 import "./utils/errorHandler";
 
 app.listen(SERVER_PORT, () => {
-    logger.info(`Server is ready on http://localhost:${SERVER_PORT}`);
+    logger.info(`Server listening on port ${SERVER_PORT}`);
 });
 
 connectDB();
